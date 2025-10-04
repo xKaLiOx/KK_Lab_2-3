6ae8b1b23792087ba877260c72f75dce429873e5
xKaLiOx <linas159159123@gmail.com>
2025-10-04 18:22:57 +0300

    feature: textbox, serial data print

--- KK_Lab2_FUNCTIONS.py
+++ KK_Lab2_FUNCTIONS.py
@@ -2,9 +2,8 @@ import tkinter as tk
 import tkinter.messagebox as messagebox
 
 import KK_Lab2_GLOBALS as globals
-#import KK_Lab2_GUI
 
-def COM_port_Open_Close(button):
+def COM_port_Open_Close(button,label,root,textbox):
     if globals.Port_connected == False:
         try:
             globals.serialPort.baudrate = globals.selected_baud_rate
@@ -12,19 +11,56 @@ def COM_port_Open_Close(button):
             globals.serialPort.parity = globals.parity_option_dict.get(globals.selected_parity)
             globals.serialPort.stopbits = int(globals.selected_stop_bits)
             globals.serialPort.bytesize = int(globals.selected_data_bits)
+            globals.serialPort.timeout = 0.01 #in seconds
+            
+            globals.serialPort.close()
             globals.serialPort.open()
             button['text'] = "Close port"
+            label['background'] = "green"
             globals.Port_connected = True
+            root.after(10, lambda:COM_port_parse_data(root,textbox))#start reading
         except Exception as e:
-            tk.messagebox.showerror("Error", f"Port can't be opened: {e}")
+            tk.messagebox.showerror("ERROR", f"Port can't be opened: {e}")
+            label['background'] = "red"
     else:
         try:
             globals.serialPort.close()
             button['text'] = "Open port"
+            label['background'] = "red"
             globals.Port_connected = False
         except Exception as e:
-            messagebox.showerror("Error", f"Port can't be closed: {e}")
+            messagebox.showerror("ERROR", f"Port can't be closed: {e}")
+            
+
 def GUI_Exit(root):
-    exit_msg = messagebox.askyesno("Exit", "Are you sure you want to exit?",icon='warning')
+    exit_msg = messagebox.askyesno("WARNING", "Are you sure you want to exit?",icon='warning')
     if exit_msg:
         root.destroy()
+        
+def COM_port_parse_data(root,textbox):
+    if globals.serialPort.in_waiting:
+        try:
+            data = globals.serialPort.readline()
+            if data:
+                com_port_log_data(data,textbox)
+        except Exception as e:
+            log_msg = f"Error reading data: {e}\n"
+            com_port_log_data(log_msg,textbox)
+    
+    
+    if globals.Port_connected:
+        root.after(10, lambda: COM_port_parse_data(root,textbox))#blocking every 10 ms while port open
+        
+def GUI_log_clear(textbox):
+    textbox['state'] = 'normal'
+    textbox.delete(1.0, tk.END)
+    textbox['state'] = 'disabled'
+    
+def com_port_log_data(data,textbox):
+    textbox['state'] = 'normal'
+    try:
+        textbox.insert(tk.END, data.decode('utf-8'))
+    except:
+        textbox.insert(tk.END, data)
+    textbox.see(tk.END)
+    textbox['state'] = 'disabled'
--- KK_Lab2_GUI.py
+++ KK_Lab2_GUI.py
@@ -12,14 +12,26 @@ root.title("Kompiuterinės komunikacijos 2 laboratorinis darbas")
 root.geometry("1000x600")
 root.iconbitmap("icon.ico")
 
-myButton_quit = tk.Button(root, text="Exit", command=lambda : Func.GUI_Exit(root),width=4, height=1,font=globals.myFontMain)
-myButton_port_settings = tk.Button(root, text="Serial Port Settings", command=lambda :KK_Lab2_UART_TAB.com_port_settings(root),padx=80,pady=10,width=4, height=1,font=globals.myFontMain)
-myButton_open_port = tk.Button(root, text="Open port", command=lambda :Func.COM_port_Open_Close(myButton_open_port))
+myButton_quit = tk.Button(root, text="Exit", command=lambda : Func.GUI_Exit(root),width=8, height=1,font=globals.myFontMain)
+myButton_port_settings = tk.Button(root, text="Serial Port Settings",width=15, height=1, command=lambda :KK_Lab2_UART_TAB.com_port_settings(root),font=globals.myFontMain)
+myButton_open_port = tk.Button(root, text="Open port",width=10, height=1, command=lambda :Func.COM_port_Open_Close(myButton_open_port,myLabel_COM_status,root,myText_COM_logs))
 
-myButton_quit.grid(row=3, column=3)
-myButton_port_settings.grid(row=3, column=0)
-myButton_open_port.grid(row=3, column=1)
+myText_COM_logs = tk.Text(root, height=20, width=60, font=globals.myFontMain, state='disabled')
+myLabel_COM_status = tk.Label(root, background="red")
+myButton_clear_Label = tk.Button(root, text="Clear data",width=8, height=1, command=lambda : Func.GUI_log_clear(myText_COM_logs),font=globals.myFontMain)
 
+tk.Label(root, text="").grid(row=0, column=0, sticky="n",columnspan=10)
+tk.Label(root, text="").grid(row=3, column=0, sticky="n",columnspan=5)
+tk.Label(root, text="").grid(row=5, column=0, sticky="n",columnspan=5)
+
+myButton_port_settings.grid(row=1, column=0)
+myButton_open_port.grid(row=1, column=1,columnspan=3)
+
+myButton_quit.grid(row=6, column=8)
+
+myLabel_COM_status.grid(row=2, column=2, sticky="ew")
+myText_COM_logs.grid(row=4, column=0, columnspan=5,padx=10)
+myButton_clear_Label.grid(row=6,column=0,columnspan=5)
 
 tk.mainloop()
         
